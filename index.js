681123ccae0ef82cb584b11dc41faaf08fd65b35
Chet25 <chirag1292003@gmail.com>
2023-02-10 18:47:28 +0530

    Meme generator It somewhat works, like a roulette wheel

--- index.js
+++ index.js
@@ -1,135 +1,82 @@
 
-const data = [
-    {
-        source: "https://source.unsplash.com/Wbu_scb-9HQ",
-        name: "The Great Wall of China",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "CHINA",
-    },
-    {
-        source: "https://source.unsplash.com/OkiDIla7K8Q",
-        name: "Christ the Redeemer",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "BRAZIL",
-    },
-    {
-        source: "https://source.unsplash.com/iWMfiInivp4",
-        name: "Taj Mahal",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "INDIA",
-    },
-    {
-        source: "https://source.unsplash.com/rFESpq5MMTg",
-        name: "Machu Pichhu",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "PERU",
-    },
-    {
-        source: "https://source.unsplash.com/JELUPXqdKDw",
-        name: "Chichen Itza",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "MEXICO",
-    },
-    {
-        source: "https://source.unsplash.com/H_u3a0rQowk",
-        name: "Colosseum",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "ROME",
-    },
-    {
-        source: "https://source.unsplash.com/py8omnp-hko",
-        name: "Petra",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "JORDAN",
-    },
-    {
-        source: "https://source.unsplash.com/7UDX3BAurng",
-        name: "The Great Pyramid of Giza",
-        mapLink: "https://www.google.co.in/maps",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non vero omnis perferendis esse at recusandae, adipisci blanditiis quibusdam commodi quam sequi ratione qui itaque voluptatibus soluta. Accusamus libero quidem provident.",
-        country: "EDYPT",
-    },
-]
-
-//
-
-
-function Navbar(){
+function Navbar() {
     return (
-        <span className = "navbar">
-            <img src="img/Fill 213.png"/>
-            <span>my travel journal.</span>
+        <span className="navbar">
+            <div>
+                <img src="https://th.bing.com/th/id/R.73efe708250aa8d2c4cfb700ab5861b9?rik=kiuNdQ8WwJ5MPg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fmeme-png-lol-meme-face-512.png&ehk=AKJy6HLsYA%2bWus72cn6HcJ9utgFu9%2b%2bqd1HxzAvlwIY%3d&risl=&pid=ImgRaw&r=0" />
+                <p className="title">Meme Generator</p>
+            </div>
+            <p>React Course - Project 3</p>
         </span>
     )
 }
 
-function DestinatonInfo({source, name, maplink, dates, description, country}){
-    return(
-        < div className = "destination-info">
-            <div className = "destination-img">
-                <img src={source}/>
+function Form() {
+    const [memeData, setMemeData] = React.useState([])
+
+    const[generatedMemeData, setGeneratedMemeData] = React.useState({
+        topText: 'Top Text',
+        bottomText: 'Bottom Text',
+        url: "https://i.imgflip.com/30b1gx.jpg"
+    })
+
+    React.useEffect(
+        function () {
+            fetch('https://api.imgflip.com/get_memes')
+                .then((res) => res.json())
+                .then((resjson) => {
+                    setMemeData(resjson.data.memes)
+                })
+        }
+        , [])
+
+    console.log(memeData)
+
+    function settingUrl() {
+        const index = Math.floor(Math.random() * memeData.length)
+        const memeUrl = memeData[index].url
+
+        setGeneratedMemeData(function(prev){
+            return {
+                ...prev,
+                url: memeUrl
+            }
+        })
+    }
+
+    function settingTopBottomText(event) {
+        const { name, value } = event.target
+        setGeneratedMemeData(function (prev) {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input type="text" placeholder="Top text" className="form-inputs" name='topText' onChange={settingTopBottomText} />
+                <input type="text" placeholder="Bottom text" className="form-inputs" name='bottomText' onChange={settingTopBottomText} />
+                <button onClick={settingUrl} className="form-button">Get a new image 🧙‍♂️</button>
             </div>
-            <span className="location-description">
-                <span className="location">📌 {country}</span>
-                {/* can't pass a variable as a link to anchor tag at the moment ,hence passing a google map link directly */}
-                <a href="https://www.google.co.in/maps" className="google-map-link">View on google maps</a>
-            
-            <h1 className="location-name">{name}</h1>
-            <p className="dates">{dates}</p>
-            <p className="about-location">{description}</p>
-            
-            </span>
-        </div>
+            <div className='meme'>
+                <img src={generatedMemeData.url} className="meme-img" />
+                <h2 className='meme-top-text'>{generatedMemeData.topText}</h2>
+                <h2 className='meme-bottom-text'>{generatedMemeData.bottomText}</h2>
+            </div>
+        </main>
     )
 }
-// 
-function Listing({data}){
-    const cards = data.map(
-        function({source, name, maplink, dates, description, country}){
-            return(
-                <DestinatonInfo 
-                    source= {source}
-                    name= {name}
-                    mapLink= {maplink}
-                    dates= {dates}
-                    description= {description}
-                    country= {country}
-                />
-            )
 
-        }
-    )
+
+function App() {
 
     return (
         <div>
-            {cards}
-        </div>
-    )
-}
-
-function App() {
-    return (
-        <div >
-            <Navbar/>
-            <Listing
-                data = {data}
-            />
-           
+            <Navbar />
+            <Form />
         </div>
     )
 }
